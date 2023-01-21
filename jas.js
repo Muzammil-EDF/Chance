b439471c75246933fc97a2bf7bb9a565f0dacc3b
Muzammil-EDF <123097477+Muzammil-EDF@users.noreply.github.com>
2023-01-21 22:11:51 +0500

    Second com

---
+++ jas.js
@@ -0,0 +1,124 @@
+
+// =====================CHALLANGES===========================
+
+
+// let me = 'mynameismuzammmil';
+// console.log(me)
+
+// =====================Challenge-1===========================
+
+// let myAge = 20
+// let humanDogRatio = 10
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// =====================Challenge-2===========================
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// =====================Challenge-3===========================
+
+// function increment(){
+//     console.log("User is clicking this button")
+// }
+
+// =====================Challenge-4===========================
+
+// function call() {
+//     console.log(42)
+// }
+// call()
+// call()
+
+// =====================Challenge-5===========================
+
+// create a function that sum the time of three laps
+
+// let lap1=30
+// let lap2=35
+// let lap3=40
+
+// function lapTimeSum(){
+//     console.log(lap1+lap2+lap3)
+// }
+// lapTimeSum()
+
+// =====================Challenge-6===========================
+
+let countEl = document.getElementById("count")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+// =====================Challenge-7===========================
+
+function save() {
+    console.log(count)
+}
+save()
+
+// STRINGS
+
+// =====================Challenge-8===========================
+
+// let username = "per"
+// let message = "you have three new notifications"
+// console.log(message + "," + username + "!")
+
+// let messageToUser = message + "," + username + "!"
+// console.log(messageToUser)
+
+
+// =====================Challenge-9===========================
+
+// let name = "muzammil"
+// let greeting = "Hi"
+// let myGreetings = name + greeting
+// console.log(myGreetings)
+
+
+// =====================Challenge-10===========================
+
+// Strings & Numbers
+
+// let p = 4
+// let bp = 10
+// console.log(p + bp)
+
+// let points = "4"
+// let bonusPoints = "10"
+// let totalPoints = points + bonusPoints
+// console.log(totalPoints)
+
+// console.log(4 + 5) // = 9
+// console.log("4" + "9") // = 49
+// console.log("5" + 1) // = 51
+// console.log(100 + "100") // = 100100
+
+
+// =====================Challenge-11===========================
+
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Muzammil"
+let greetings = "Welcome back I hope you are fine "
+welcomeEl.innerText = greetings + name
+welcomeEl.innerText += " emoji"
+
+
+
+
+
